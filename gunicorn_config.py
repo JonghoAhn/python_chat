778df75cffd8b55c7a572dff3563beaa32c138cb
JonghoAhn <145340586+JonghoAhn@users.noreply.github.com>
2024-06-02 21:10:31 +0900

    dd

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 command = 'gunicorn'
-pythonpath = 'https://port-0-python-chat-1272llwx73p0i.sel5.cloudtype.app/'
+pythonpath = 'https://port-0-python-chat-1272llwx73p0i.sel5.cloudtype.app/chat'
 workers = 4  # CPU 코어 수에 따라 조정
 threads = 2
 timeout = 120  # 타임아웃 값을 120초로 설정
