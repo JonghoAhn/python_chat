b0ec08b3c2cf9c9b27560ef36db833905dae7a2b
JonghoAhn <145340586+JonghoAhn@users.noreply.github.com>
2024-06-02 16:08:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    return render_template("chat.html")
+
+@socketio.on("event")
+def event_handler(json):
+    if "data" in json:
+        if json["data"] == "Connect":
+            socketio.emit("response", {"nickname": "", "message": "새로운 유저 입장"})
+    else:
+        nickname = json["nickname"].encode("utf-8").decode("utf-8")
+        message = json["message"].encode("utf-8").decode("utf-8")
+        socketio.emit("response", {"nickname": nickname, "message": message})
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, allow_unsafe_werkzeug=True)
