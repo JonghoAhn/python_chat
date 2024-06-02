c57e96cfc259ba7252a84c35c95c7ab8c76f6aef
JonghoAhn <145340586+JonghoAhn@users.noreply.github.com>
2024-06-02 22:45:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,29 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-from flask_socketio import SocketIO, emit, join_room, leave_room
-from flask_cors import CORS
-
+from flask_socketio import SocketIO, emit, leave_room,join_room
+import os
 
 app = Flask(__name__)
-CORS(app,supports_credentials=True)
-app.config['SECRET_KEY'] = 'mosan'
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-
-
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_key')  # 환경변수에서 비밀키를 가져오거나 기본값 사용
+socketio = SocketIO(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         session['username'] = request.form['username']
-        session['room'] = 'default_room'
         return redirect(url_for('chat'))
     return render_template('index.html')
 
 @app.route('/chat')
 def chat():
-    if 'username' not in session:
+    username = session.get('username')
+    if not username:
         return redirect(url_for('index'))
-    return render_template('chat.html', username=session['username'], room=session['room'])
-
-@socketio.on('join', namespace='/chat')
-def join(message):
-    username = message['username']
-    room = message['room']
-    join_room(room)
-    emit('status', {'msg': username + ' has joined the room.'}, room=room)
+    return render_template('chat.html', username=username)
 
 @socketio.on('message', namespace='/chat')
 def message(message):
-    emit('message', {'msg': message['username'] + ': ' + message['msg']}, room=message['room'])
-
-@socketio.on('leave', namespace='/chat')
-def leave(message):
-    username = message['username']
-    room = message['room']
-    leave_room(room)
-    emit('status', {'msg': username + ' has left the room.'}, room=room)
+    emit('message', {'msg': f"{message['username']}: {message['msg']}"}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
