5495094bc0dc8b6817b45fda88d653d21cdc4d5c
JonghoAhn <145340586+JonghoAhn@users.noreply.github.com>
2024-06-02 21:19:56 +0900

    dd

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO, leave_room, emit, join_room
 from flask_cors import CORS
-
+import eventlet
+import eventlet.wsgi
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-app.config['SECRET_KEY'] = 'mosan'
-socketio = SocketIO(app, cors_allowed_origins="*")
 
+app.config['SECRET_KEY'] = 'your_secret_key'  # 보안을 위해 환경 변수에서 불러오도록 설정
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+# CORS 정책 설정
+CORS(app, resources={r"/*": {"origins": "*"}})
 
+# SocketIO 초기화
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,25 +27,27 @@ def index():
 def chat():
     if 'username' not in session:
         return redirect(url_for('index'))
-    return render_template('chat.html', username=session['username'], room=session['room'])
+    username = session['username']
+    room = session['room']
+    return render_template('chat.html', username=username, room=room)
 
 @socketio.on('join', namespace='/chat')
 def join(message):
     username = message['username']
     room = message['room']
     join_room(room)
-    emit('status', {'msg': username + ' has joined the room.'}, room=room)
+    emit('status', {'msg': f'{username} has joined the room.'}, room=room)
 
 @socketio.on('message', namespace='/chat')
 def message(message):
-    emit('message', {'msg': message['username'] + ': ' + message['msg']}, room=message['room'])
+    emit('message', {'msg': f"{message['username']}: {message['msg']}"}, room=message['room'])
 
 @socketio.on('leave', namespace='/chat')
 def leave(message):
     username = message['username']
     room = message['room']
     leave_room(room)
-    emit('status', {'msg': username + ' has left the room.'}, room=room)
+    emit('status', {'msg': f'{username} has left the room.'}, room=room)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
